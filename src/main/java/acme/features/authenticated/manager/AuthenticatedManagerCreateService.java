ac8fb0dc8561ddc835801f42d6867abacfd67962
Migueel0 <miggaller@alum.us.es>
2025-06-24 19:33:33 +0200

    feat: Add create service

---
+++ src/main/java/acme/features/authenticated/manager/AuthenticatedManagerCreateService.java
@@ -0,0 +1,67 @@
+
+package acme.features.authenticated.manager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.flight.Flight;
+import acme.realms.manager.Manager;
+
+@GuiService
+public class AuthenticatedManagerCreateService extends AbstractGuiService<Manager, Flight> {
+
+	@Autowired
+	private AuthenticatedManagerRepository repository;
+
+
+	@Override
+	public void authorise() {
+		String method = super.getRequest().getMethod();
+		boolean status = true;
+		if (method.equals("POST")) {
+			int id = super.getRequest().getData("id", int.class);
+			status = id == 0;
+
+		}
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		Flight flight;
+		Manager manager;
+
+		manager = (Manager) super.getRequest().getPrincipal().getActiveRealm();
+
+		flight = new Flight();
+		flight.setDraftMode(true);
+		flight.setManager(manager);
+
+		super.getBuffer().addData(flight);
+	}
+
+	@Override
+	public void bind(final Flight flight) {
+
+		super.bindObject(flight, "tag", "requiresSelfTransfer", "cost", "description");
+
+	}
+
+	@Override
+	public void perform(final Flight flight) {
+		this.repository.save(flight);
+	}
+
+	@Override
+	public void unbind(final Flight flight) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(flight, "tag", "requiresSelfTransfer", "cost", "description");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
