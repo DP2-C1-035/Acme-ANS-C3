940a85fa2a152fd994aa9c4721743da209ab11b5
Migueel0 <miggaller@alum.us.es>
2025-06-24 09:07:21 +0200

    feat:Flight entity

---
+++ src/main/java/acme/entities/flight/Flight.java
@@ -0,0 +1,60 @@
+
+package acme.entities.flight;
+
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import acme.realms.Manager;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(indexes = {
+	@Index(columnList = "draftMode")
+})
+public class Flight extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(min = 1, max = 50)
+	@Automapped
+	private String				tag;
+
+	@Mandatory
+	@Automapped
+	private boolean				requiresSelfTransfer;
+
+	@Mandatory
+	@ValidMoney(min = 0, max = 200000)
+	@Automapped
+	private Money				cost;
+
+	@Optional
+	@ValidString(min = 0, max = 255)
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@Automapped
+	private boolean				draftMode;
+
+	// Relationships ----------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Manager				manager;
+}
