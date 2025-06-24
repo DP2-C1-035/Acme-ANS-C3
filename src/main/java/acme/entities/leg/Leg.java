c2c24a6bfe7f2a766d6674fb935c482173f3d149
Migueel0 <miggaller@alum.us.es>
2025-06-24 09:23:27 +0200

    Feat: Add leg information

---
+++ src/main/java/acme/entities/leg/Leg.java
@@ -0,0 +1,96 @@
+
+package acme.entities.leg;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.entities.aircraft.Aircraft;
+// import acme.entities.airport.Airport;
+import acme.entities.flight.Flight;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(indexes = {
+	//Feature Leg
+	@Index(columnList = "flight_id,draftMode,scheduledDeparture,scheduledArrival"), // 
+	@Index(columnList = "aircraft_id,draftMode,scheduledDeparture,scheduledArrival"),// 
+	@Index(columnList = "flight_id,draftMode,scheduledArrival"),// 
+	@Index(columnList = "flight_id,draftMode,scheduledDeparture"), // 
+	@Index(columnList = "flight_id,draftMode"),//
+	//Feature Claim
+	@Index(columnList = "flight_id,draftMode,scheduledArrival,aircraft_id"),//
+	@Index(columnList = "draftMode")//
+
+})
+public class Leg extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{3}\\d{4}$")
+	@Column(unique = true)
+	private String				flightNumber;
+
+	@Mandatory
+	@ValidMoment(past = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				scheduledDeparture;
+
+	@Mandatory
+	@ValidMoment(past = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				scheduledArrival;
+
+	@Mandatory
+	@Automapped
+	private boolean				draftMode;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private LegStatus			status;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Aircraft			aircraft;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Flight				flight;
+
+	/*
+	 * Uncomment when Airport entity is done
+	 * 
+	 * @Mandatory
+	 * 
+	 * @Valid
+	 * 
+	 * @ManyToOne(optional = false)
+	 * private Airport departureAirport;
+	 * 
+	 * @Mandatory
+	 * 
+	 * @Valid
+	 * 
+	 * @ManyToOne(optional = false)
+	 * private Airport arrivalAirport;
+	 */
+}
