12e2c7f5efbac9ad4830e4782535e3b105ec693b
paulalunanavarro <paulunnav@alum.us.es>
2025-06-25 18:05:54 +0200

    feat(): relaciones con flightAssignment

--- src/main/java/acme/entities/flight_assignment/FlightAssignment.java
+++ src/main/java/acme/entities/flight_assignment/FlightAssignment.java
@@ -4,6 +4,9 @@ package acme.entities.flight_assignment;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -14,13 +17,17 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
+import acme.entities.flight_crew_member.FlightCrewMember;
+import acme.entities.leg.Leg;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-
+@Table(indexes = {
+	@Index(columnList = "draftMode"), @Index(columnList = "draftMode, leg_id"), @Index(columnList = "assignmentStatus, flight_crew_member_id")
+})
 public class FlightAssignment extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
@@ -45,19 +52,19 @@ public class FlightAssignment extends AbstractEntity {
 	@Automapped
 	private String				remarks;
 
-	//@Mandatory
+	@Mandatory
 	// HINT: @Valid by default.
-	//@Automapped
-	//private boolean				draftMode;
+	@Automapped
+	private boolean				draftMode;
 
-	//@Mandatory
-	//@Valid
-	//@ManyToOne(optional = false)
-	//private FlightCrewMember	flightCrewMember;
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private FlightCrewMember	flightCrewMember;
 
-	//@Mandatory
-	//@Valid
-	//@ManyToOne(optional = false)
-	//private Leg					leg;
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Leg					leg;
 
 }
