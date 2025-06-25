5fd493e6115e6f4394c637bf4e7f64222a6818f9
Migueel0 <miggaller@alum.us.es>
2025-06-25 20:07:37 +0200

    feat: Delete flight

--- src/main/java/acme/features/manager/flight/ManagerFlightController.java
+++ src/main/java/acme/features/manager/flight/ManagerFlightController.java
@@ -24,6 +24,15 @@ public class ManagerFlightController extends AbstractGuiController<Manager, Flig
 	@Autowired
 	ManagerShowFlightService			showService;
 
+	@Autowired
+	private ManagerPublishFlightService	publishService;
+
+	@Autowired
+	private ManagerUpdateFlightService	updateService;
+
+	@Autowired
+	private ManagerDeleteFlightService	deleteService;
+
 	// Constructors -----------------------------------------------------------
 
 
@@ -32,6 +41,9 @@ public class ManagerFlightController extends AbstractGuiController<Manager, Flig
 		super.addBasicCommand("create", this.createService);
 		super.addBasicCommand("list", this.listService);
 		super.addBasicCommand("show", this.showService);
+		super.addBasicCommand("update", this.updateService);
+		super.addBasicCommand("delete", this.deleteService);
+		super.addCustomCommand("publish", "update", this.publishService);
 	}
 
 }
